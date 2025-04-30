08933702dd6abea4f4e3196ddbb10ab21a63279b
Mihalis Koutouvos <mihaliskoutouvos@gmail.com>
2025-04-30 13:56:56 -0400

    Added read csv feature

--- web_scraper.py
+++ web_scraper.py
@@ -1,8 +1,16 @@
 import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 
-#Testing the Goodreads page:
+#Read in the csv data:
+df = pd.read_csv('goodreads_list.csv')
+print(df.head())
+
+#We want to find each book on the Goodreads website, so a url will be needed
+for i in df:
+
+
+
+#Now let's begin searching the Goodreads website using GET requests
+#Beautiful soup will then be used to parse the HTML
 goodreads_request_test = requests.get("https://www.goodreads.com/")
-
-#Checking the status code:
-print(goodreads_request_test)
-print(goodreads_request_test.content)
